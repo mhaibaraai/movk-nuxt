4c6861dda0a77ff2c1afee713424944a2636c05f
mhaibaraai <mhaibaraai@gmail.com>
2025-11-12 11:41:00 +0800

    📚 docs: 添加文档网站基础结构
    - 基于 Nuxt Content 构建的文档系统
    - 使用 @movk/nuxt-docs 主题扩展
    - 包含组件文档自动生成功能

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+  extends: ['@movk/nuxt-docs'],
+  css: ['~/assets/css/main.css'],
+  routeRules: {
+    // redirects - default root pages
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+  compatibilityDate: 'latest'
+})
