456b661dcdb1cdfcdd211af2af53dac16afcacfb
mhaibaraai <mhaibaraai@gmail.com>
2026-03-09 18:00:36 +0800

    chore(docs): 重新添加 reka-ui 的 transpile 配置

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -23,6 +23,10 @@ export default defineNuxtConfig({
     name: 'Movk Nuxt'
   },
 
+  build: {
+    transpile: ['reka-ui']
+  },
+
   routeRules: {
     '/docs': { redirect: '/docs/getting-started', prerender: false },
     '/docs/components': { redirect: '/docs/components/with-character-limit', prerender: false },
